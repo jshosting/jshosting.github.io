613079c4436f0ab234f37ab5b03bb393f90697ee
jshosting <105516957+jshosting@users.noreply.github.com>
2023-05-05 10:36:24 -0700

    Delete c directory

--- c/php/hostGame.php
+++
@@ -1,6 +0,0 @@
-<?php
-	if (!$gameName = $_POST["id"]) return false;
-	if (strpos($gameName, "\\") !== false || strpos($gameName, ".") !== false || strpos($gameName, "*") !== false) return false;
-	if (!fclose(fopen("../games/".$gameName, "x"))) return false;
-	echo $gameName;
-?>
--- c/php/waitForStart.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if (!$gameName = $_POST["id"]) return false;
-	if (!$file = fopen("../games/".$gameName, "r")) return false;
-	$line = fgets($file);
-	fclose($file);
-	echo $line;
-?>
